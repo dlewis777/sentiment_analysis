1db53dabdfa759173e7a406b71a7879f04185544
Dylan Lewis <dlewis77@br006.pvt.bridges.psc.edu>
2017-06-20 15:24:50 -0400

    lexicon code

--- lexicon.py
+++ lexicon.py
@@ -1,10 +1,8 @@
-import scikit-learn
-import nltk
 import sys
 
 def read_lexicon(filename):
 
-    ldict = []
+    ldict = {}
     with open(filename,'r') as lexicon:
         for line in lexicon:
             ldict[line.split()[0]] = line.split()[2]
@@ -13,12 +11,12 @@ def read_lexicon(filename):
 
 def read_file(filename):
 
-    examples = []
+    examples = {}
     
     with open(filename,'r') as data:
         
         for line in data:
-            examples[line.split()[8:]] = line.split()[7]
+            examples[tuple(line.split()[8:])] = line.split()[7]
 
     return examples
 
@@ -29,17 +27,19 @@ def get_counts(data, lex):
         true = data[example]
         for word in example:
             if word in lex:
-                score += lex[word]
+                print lex[word]
+                score += float(lex[word])
+                print score
         score = score / len(example)
         score = round((score * 2) + 3)
         data[example] = (score,true)
 
-if __name__ == __main__:
+if __name__ == "__main__":
 
     lex = read_lexicon(sys.argv[1])
     data = read_file(sys.argv[2])
     get_counts(data,lex)
     
-    for example in data:
-        print data[example]
+    #for example in data:
+    #    print data[example]
 
