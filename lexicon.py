254cf77ef749ddbb22b4fb2095c278da20611785
Dylan Lewis <dlewis77@br006.pvt.bridges.psc.edu>
2017-06-20 15:51:48 -0400

    lexicon update

--- lexicon.py
+++ lexicon.py
@@ -1,5 +1,5 @@
 import sys
-
+import scipy.stats
 def read_lexicon(filename):
 
     ldict = {}
@@ -16,30 +16,43 @@ def read_file(filename):
     with open(filename,'r') as data:
         
         for line in data:
-            examples[tuple(line.split()[8:])] = line.split()[7]
+            examples[tuple(map(lambda x: x.lower(), line.split()[8:]))] = line.split()[7]
 
     return examples
 
 def get_counts(data, lex):
 
+    x = []
+    y = []
+    num = 0
     for example in data:
+
+        counter = 0.
         score = 0
         true = data[example]
         for word in example:
             if word in lex:
-                print lex[word]
+                counter += 1
+                #print "score:" , lex[word]
                 score += float(lex[word])
-                print score
-        score = score / len(example)
+        #print example        
+        score = score / float(counter)
+        print "avg score:" , score , true
         score = round((score * 2) + 3)
         data[example] = (score,true)
-
+        x.append(score)
+        y.append(float(true))
+        num += 1
+        print num
+    return x,y
 if __name__ == "__main__":
 
     lex = read_lexicon(sys.argv[1])
     data = read_file(sys.argv[2])
-    get_counts(data,lex)
+    x,y = get_counts(data,lex)
+    
     
     #for example in data:
     #    print data[example]
 
+    print scipy.stats.pearsonr(x,y)
